df504bbd46800804f316e7bb4078244abc6c30e9
Etilrem2 <123809682+Etilrem2@users.noreply.github.com>
2023-01-28 16:20:54 +0100

    Add files via upload

--- pages/rating.py
+++ pages/rating.py
@@ -6,6 +6,9 @@ import pandas as pd
 from funksjoner.elo import expected, elo
 import datetime
 import dash_core_components as dcc
+from email.message import EmailMessage
+import ssl
+import smtplib
 
 dash.register_page(__name__, name='Spillerrating')
 
@@ -90,12 +93,56 @@ def update_countdown(n):
     time_until_end = end_of_month - today
     time_until_end_seconds = int(time_until_end.total_seconds())
 
-    if time_until_end_seconds < 3600:
+    if time_until_end_seconds < 200365:
+
+        e = {}
+
+        with open("Poeng/Elo.txt", 'r') as f:
+            for linje in f:
+                (spiller, rating) = linje.split()
+                e[spiller] = rating
+
+        f.close()
+
+        df = pd.DataFrame.from_dict(e, orient='index').reset_index()
+        df.columns = 'Spiller', 'Rating'
+
+        kolonner = df.columns.tolist()
+
+        df['Rating'] = df['Rating'].astype('int')
+        df.sort_values(by='Rating', inplace=True, ascending=False)
+
+        vinner = df.Spiller.iloc[0]
+
+
+
+        email_sender = 'erlendtilrem@gmail.com'
+        email_password = 'mwdowhkjqumbhssw'
+        email_reciever = 'etilrem@hotmail.com'
+
+
+        subject = 'Vinneren er....'
+        body = f"""
+        Denne månedens vinner er: {str(vinner)}!
+        """
+
+        em = EmailMessage()
+        em['From'] = email_sender
+        em['To'] = email_reciever
+        em['Subject'] = subject
+        em.set_content(body)
+
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(email_sender, email_password)
+            smtp.sendmail(email_sender, email_reciever, em.as_string())
+
         with open('Poeng/Elo.txt', 'w') as f:
             e = {'Eigil': 1000, 'Erlend': 1000, 'Jonas': 1000, 'Kjetil': 1000}
 
             for key, value in e.items():
                 f.write(key + ' ' + str(value) + '\n')
+        f.close()
 
     return 'Tid til reset: {} dager, {} timer, {} minutt, {} sekund'.format(time_until_end.days,
                                                                             time_until_end.seconds // 3600,
